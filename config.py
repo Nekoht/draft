051c623b1769fc2fbcf48207ac12d98c3a673dd8
Nekoht <99998062+Nekoht@users.noreply.github.com>
2022-03-09 22:44:57 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = '5136679649:AAFAJkd10oKYaRMc2knQxDsNIUpQj-UoIgc' #token de tu bot
+API_ID = 13233271 # lo creas en https://my.telegram.org/
+API_HASH = 'e3ce8145aa657c2a4cc5cf0f7183e476'  # lo creas en https://my.telegram.org/
+OWNER = [945671168]
+
+# DATOS DEL MOODLE
+USUARIO = 'melissa.suarez@estudiantes.fbio.uh.cu'
+MOODLE_PASSWORD = 'Melissiña.2323'
+USUARIO_ID =  5887 #Tu id de la nube
+ZIP_MB = 100
+MOODLE_URL = 'evea.uh.cu'
+
+# CUENTA DE MEGA - este bot sirve par mega tambien
+GMAIL_MEGA = '@gmail.com' 
+PASS_MEGA = 'contraseña'
